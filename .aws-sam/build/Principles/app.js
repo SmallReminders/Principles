3878b64aad91c2e898b7ad1e3ea4dd82c7966246
Elena Mikhaylova <Lena@Mac.lan>
2020-05-03 21:35:46 -0400

    read principles working with aws sam

---
+++ .aws-sam/build/Principles/app.js
@@ -0,0 +1,54 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+'use strict';
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ */
+
+/*
+ * Dependencies
+ */
+require('dotenv').config();
+require('./schema/Principle'); // mongoose schema
+const defaultPrinciples = require('./const/sample');
+
+// Mongo setup
+const mongoDB = require('./utils/mongoDB');
+mongoDB.connect();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const Principle = mongoose.model('Principle');
+
+exports.lambdaHandler = async (event, context) => {
+  try {
+    let payload;
+    if (!event.pathParameters || !('uid' in event.pathParameters)) {
+    // if no uid given, return default principles
+      payload = { principles: defaultPrinciples };
+    }
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello world',
+        payload
+        // location: ret.data.trim()
+      })
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
+};
